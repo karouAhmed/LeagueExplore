803694ca83ffb11fddba0313e880801ce8e3d02d
ahmed <ahmed@bilog.fr>
2023-10-10 17:23:59 +0200

    Initial Commit

---
+++ LeagueExplore/LeagueExploreApp.swift
@@ -0,0 +1,17 @@
+//
+//  LeagueExploreApp.swift
+//  LeagueExplore
+//
+//  Created by narfk on 10/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct LeagueExploreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
